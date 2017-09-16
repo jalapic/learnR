d742d7f133d7d1a85c65365206f367ad478f5a0c
James Curley <jc3181@columbia.edu>
2017-09-15 20:25:27 -0500

    Update 102_ggjoy.R

--- ggplot/102_ggjoy.R
+++ ggplot/102_ggjoy.R
@@ -1,53 +1,53 @@
-## ggjoy - multiple density plots
+## ggridgelines - multiple density plots
 
 ## taken from vignette at https://github.com/clauswilke/ggjoy
 
-install.packages("ggjoy")
-library(ggjoy)
+install.packages("ggridges")
+library(ggridges)
 
 
 # Density joyplots
 
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy()
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy2()  #slightly nicer imo
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges()
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges2()  #slightly nicer imo
 
 # if y-axis is categorical, no need for group. If numeric  - needs group.
 # modified dataset that represents species as a number
 
 iris$Species_num <- as.numeric(iris$Species)
-ggplot(iris, aes(x = Sepal.Length, y = Species_num, group = Species_num)) + geom_joy2()
+ggplot(iris, aes(x = Sepal.Length, y = Species_num, group = Species_num)) + geom_density_ridges2()
 
 # cut off trailing tails
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy2(rel_min_height = 0.01)
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges2(rel_min_height = 0.01)
 
 
 #Smaller scale values create a separation between the curves, and larger values create more overlap.
 
 # scale = 0.9, not quite touching
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy(scale = 0.9)
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges(scale = 0.9)
 
 # scale = 1, exactly touching
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy(scale = 1)
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges(scale = 1)
 
 # scale = 5, substantial overlap
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy(scale = 5)
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges(scale = 5)
 
 
 
 ##  ggjoy has its own theme that helps make things look nicer:
-ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy() + theme_joy()
+ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_density_ridges() + theme_ridges()
 
 
 # also  use 'expand' helps the labels and text on axis look nicer: 
 ggplot(iris, aes(x = Sepal.Length, y = Species)) + 
-  geom_joy() + theme_joy() +
+  geom_density_ridges() + theme_ridges() +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_y_discrete(expand = c(0.01, 0))
 
 
 # remove background grid if desired:
 ggplot(iris, aes(x = Sepal.Length, y = Species)) + 
-  geom_joy() + theme_joy(grid = FALSE) +
+  geom_density_ridges() + theme_ridges(grid = FALSE) +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_y_discrete(expand = c(0.01, 0))
 
@@ -57,20 +57,20 @@ ggplot(iris, aes(x = Sepal.Length, y = Species)) +
 ## Cyclical scales
 
 ggplot(diamonds, aes(x = price, y = cut, fill = cut)) + 
-  geom_joy(scale = 4) 
+  geom_density_ridges(scale = 4) 
 
 ggplot(diamonds, aes(x = price, y = cut, fill = cut)) + 
-  geom_joy(scale = 4) + 
+  geom_density_ridges(scale = 4) + 
   scale_fill_cyclical(values = c("blue", "green")) # will cycle between 2 colors given
 
 ggplot(diamonds, aes(x = price, y = cut, fill = cut)) + 
-  geom_joy(scale = 4) + 
+  geom_density_ridges(scale = 4) + 
   scale_fill_cyclical(values = c("blue", "green", "pink")) # will cycle between 3 colors given
 
 
 ## Can also  plot mini-histograms
 ggplot(iris, aes(x = Sepal.Length, y = Species, height = ..density..)) + 
-  geom_joy(stat = "binline", bins = 20, scale = 0.95, draw_baseline = FALSE)
+  geom_density_ridges(stat = "binline", bins = 20, scale = 0.95, draw_baseline = FALSE)
 
 
 
@@ -82,8 +82,8 @@ ggplot(iris, aes(x = Sepal.Length, y = Species, height = ..density..)) +
 
 library(ggplot2movies)
 ggplot(movies[movies$year>1912,], aes(x = length, y = year, group = year)) +
-  geom_joy(scale = 10, size = 0.25, rel_min_height = 0.03) +
-  theme_joy() +
+  geom_density_ridges(scale = 10, size = 0.25, rel_min_height = 0.03) +
+  theme_ridges() +
   scale_x_continuous(limits=c(1, 200), expand = c(0.01, 0)) +
   scale_y_reverse(breaks=c(2000, 1980, 1960, 1940, 1920, 1900), expand = c(0.01, 0))
 
@@ -96,7 +96,7 @@ library(forcats)
 Catalan_elections %>%
   mutate(YearFct = fct_rev(as.factor(Year))) %>%
   ggplot(aes(y = YearFct)) +
-  geom_joy(aes(x = Percent, fill = paste(YearFct, Option)), 
+  geom_density_ridges(aes(x = Percent, fill = paste(YearFct, Option)), 
            alpha = .8, color = "white", from = 0, to = 100) +
   labs(x = "Vote (%)",
        y = "Election Year",
@@ -119,13 +119,13 @@ Catalan_elections %>%
 
 library(viridis)
 ggplot(lincoln_weather, aes(x = `Mean Temperature [F]`, y = `Month`, fill = ..x..)) +
-  geom_joy_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01, gradient_lwd = 1.) +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_y_discrete(expand = c(0.01, 0)) +
   scale_fill_viridis(name = "Temp. [F]", option = "C") +
   labs(title = 'Temperatures in Lincoln NE',
        subtitle = 'Mean temperatures (Fahrenheit) by month for 2016\nData: Original CSV from the Weather Underground') +
-  theme_joy(font_size = 13, grid = TRUE) + theme(axis.title.y = element_blank())
+  theme_ridges(font_size = 13, grid = TRUE) + theme(axis.title.y = element_blank())
 
 
 
@@ -140,7 +140,7 @@ pois_data$value <- rpois(nrow(pois_data), pois_data$mean)
 
 # make plot
 ggplot(pois_data, aes(x = value, y = group, group = group)) +
-  geom_joy2(aes(fill = group), stat = "binline", binwidth = 1, scale = 0.95) +
+  geom_density_ridges2(aes(fill = group), stat = "binline", binwidth = 1, scale = 0.95) +
   geom_text(stat = "bin",
             aes(y = group + 0.95*(..count../max(..count..)),
                 label = ifelse(..count..>0, ..count.., "")),
@@ -153,7 +153,7 @@ ggplot(pois_data, aes(x = value, y = group, group = group)) +
   labs(title = "Poisson random samples with different means",
        subtitle = "sample size n=10") +
   guides(y = "none") +
-  theme_joy(grid = FALSE) +
+  theme_ridges(grid = FALSE) +
   theme(axis.title.x = element_text(hjust = 0.5),
         axis.title.y = element_text(hjust = 0.5))
 
